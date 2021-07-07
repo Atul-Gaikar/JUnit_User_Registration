923105352cb620d0bebdd293af2dace36b0cc9fe
Atul <atulgaikarag9@gmail.com>
2021-07-07 14:43:42 +0530

    Adding Enter A Valid First Name

---
+++ src/test/java/com/bridgelabz/junituserregistration/JUnitUserRegistrationTest.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.junituserregistration;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class JUnitUserRegistrationTest {
+
+	@Test
+	public void Firstname() {
+		JUnitUserRegistration registration = new JUnitUserRegistration();
+		boolean check = registration.FirstName();
+		assertTrue(check);
+	}
+
+//	@Test
+//	public void LastName() {
+//		JUnitUserRegistration registration = new JUnitUserRegistration();
+//		boolean check = registration.LastName();
+//		assertTrue(check);
+//	}
+//
+//	@Test
+//	public void Email() {
+//		JUnitUserRegistration registration = new JUnitUserRegistration();
+//		boolean check = registration.Email();
+//		assertTrue(check);
+//	}
+//
+//	@Test
+//	public void MobileFormat() {
+//		JUnitUserRegistration registration = new JUnitUserRegistration();
+//		boolean check = registration.MobileFormat();
+//		assertTrue(check);
+//	}
+//
+//	@Test
+//	public void PasswordFormat() {
+//		JUnitUserRegistration registration = new JUnitUserRegistration();
+//		boolean check = registration.Password();
+//		assertTrue(check);
+//	}
+}
